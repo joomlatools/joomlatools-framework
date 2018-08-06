053f054bcc26139cd1bc3b7a8aab3a6173d6035a
Johan Janssens <johan@timble.net>
2018-08-06 17:12:08 +0200

    #174 : Fix typo

--- code/plugins/system/joomlatools/joomlatools.php
+++ code/plugins/system/joomlatools/joomlatools.php
@@ -260,7 +260,7 @@ class PlgSystemJoomlatools extends JPlugin
      */
     public function onBeforeCompileHead()
     {
-        $this->_proxyEvent('onBeforeApplicaitonCompileHead');
+        $this->_proxyEvent('onBeforeApplicationCompileHead');
     }
 
     /**
