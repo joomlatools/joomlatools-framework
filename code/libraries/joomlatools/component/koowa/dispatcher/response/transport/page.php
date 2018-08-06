a4d0836afc7caa2baf94e3260c596a2abb9a202a
Johan Janssens <johan@timble.net>
2018-08-06 17:12:49 +0200

    #174 : Cleanup

--- code/libraries/joomlatools/component/koowa/dispatcher/response/transport/page.php
+++ code/libraries/joomlatools/component/koowa/dispatcher/response/transport/page.php
@@ -8,15 +8,52 @@
  */
 
 /**
- * Http Dispatcher Response Transport
+ * Page Dispatcher Response Transport
  *
  * Pass all 'html' GET requests rendered outside of 'koowa' context on to Joomla.
  *
  * @author  Johan Janssens <https://github.com/johanjanssens>
  * @package Koowa\Component\Koowa\Dispatcher\Response\Transport
  */
-class ComKoowaDispatcherResponseTransportHttp extends KDispatcherResponseTransportHttp
+class ComKoowaDispatcherResponseTransportPage extends KDispatcherResponseTransportHttp
 {
+    /**
+     * The page context
+     *
+     * @var string
+     */
+    protected $_context;
+
+    /**
+     * Constructor.
+     *
+     * @param KObjectConfig $config 	An optional KObjectConfig object with configuration options.
+     */
+    public function __construct(KObjectConfig $config)
+    {
+        parent::__construct($config);
+
+        $this->_context = $config->context;
+    }
+
+    /**
+     * Initializes the options for the object
+     *
+     * Called from {@link __construct()} as a first step of object instantiation.
+     *
+     * @param 	KObjectConfig $config 	An optional ObjectConfig object with configuration options.
+     * @return 	void
+     */
+    protected function _initialize(KObjectConfig $config)
+    {
+        $config->append(array(
+            'priority' => self::PRIORITY_NORMAL,
+            'context'  => 'koowa'
+        ));
+
+        parent::_initialize($config);
+    }
+
     /**
      * Send HTTP response
      *
@@ -29,19 +66,25 @@ class ComKoowaDispatcherResponseTransportHttp extends KDispatcherResponseTranspo
 
         if(!$response->isDownloadable() && $request->getFormat() == 'html')
         {
-            if ($request->getHeaders()->has('X-Flush-Response'))
+            $context = $this->getContext();
+
+            //Force to use system template
+            if($context == 'koowa')
             {
-                $layout = 'koowa';
+                $app = JFactory::getApplication();
+
+                if ($app->isSite()) {
+                    $app->setTemplate('system');
+                }
             }
-            else $layout = $request->query->get('tmpl', 'cmd') == 'koowa' ? 'koowa' : 'joomla';
 
             //Render the page
             $this->getObject('com:koowa.controller.page',  array('response' => $response))
-                ->layout($layout)
+                ->layout($context)
                 ->render();
 
             //Pass back to Joomla
-            if ($request->isGet() && $layout != 'koowa')
+            if ($request->isGet() && $context != 'koowa')
             {
                 //Mimetype
                 JFactory::getDocument()->setMimeEncoding($response->getContentType());
@@ -101,5 +144,15 @@ class ComKoowaDispatcherResponseTransportHttp extends KDispatcherResponseTranspo
         }
 
         return parent::send($response);
+    }
+
+    /**
+     * The page context
+     *
+     * @return string
+     */
+    public function getContext()
+    {
+        return $this->_context;
     }
 }
