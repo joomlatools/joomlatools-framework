de9c40429332585c23712b9f665975507f7e42c5
Johan Janssens <johan@janssens.me>
2020-08-24 00:36:47 +0200

    #417 - getEtag() should return string or null

--- code/libraries/joomlatools/library/dispatcher/request/abstract.php
+++ code/libraries/joomlatools/library/dispatcher/request/abstract.php
@@ -886,15 +886,12 @@ abstract class KDispatcherRequestAbstract extends KControllerRequest implements 
      *
      * @link https://tools.ietf.org/html/rfc7232#page-14
      *
-     * @return array The entity tags
+     * @return string|null The entity tag, or null if the etag header doesn't exist
      */
     public function getETag()
     {
-        $result = array();
-        if($this->_headers->has('If-None-Match'))
+        if($result = $this->_headers->get('If-None-Match'))
         {
-            $result = $this->_headers->get('If-None-Match');
-
             //Remove the encoding from the etag
             //
             //RFC-7232 explicitly states that ETags should be content-coding aware
