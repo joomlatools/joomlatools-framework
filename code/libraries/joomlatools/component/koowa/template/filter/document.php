24b056b2e9b44b1f187bb9b6fb588fe3779d14a2
Ercan Ozkaya <ercan@timble.net>
2021-02-09 14:59:45 +0300

    #389: Guard against undefined variables

--- code/libraries/joomlatools/component/koowa/template/filter/document.php
+++ code/libraries/joomlatools/component/koowa/template/filter/document.php
@@ -63,6 +63,7 @@ class ComKoowaTemplateFilterDocument extends KTemplateFilterAbstract
                 // Copied from \Joomla\CMS\Document\Renderer\Html\MetasRenderer::render
                 if (version_compare(JVERSION, '4.0', '>=')) {
                     $wa  = $document->getWebAssetManager();
+                    $wc  = [];
                     foreach ($wa->getAssets('script', true) as $asset) {
                         if ($asset instanceof \Joomla\CMS\WebAsset\WebAssetAttachBehaviorInterface) {
                             $asset->onAttachCallback($document);
@@ -135,23 +136,29 @@ class ComKoowaTemplateFilterDocument extends KTemplateFilterAbstract
             if (isset($head['assetManager']) && isset($head['assetManager']['assets'])) {
                 $manager = $head['assetManager']['assets'];
 
-                /** @var \Joomla\CMS\WebAsset\WebAssetItemInterface $script */
-                foreach ($manager['script'] as $script) {
-                    if ($script->getOption('webcomponent')) {
-                        continue; // they are loaded by Joomla in core.js
+                if (isset($manager['script'])) {
+                    /** @var \Joomla\CMS\WebAsset\WebAssetItemInterface $script */
+                    foreach ($manager['script'] as $script) {
+                        if ($script->getOption('webcomponent')) {
+                            continue; // they are loaded by Joomla in core.js
+                        }
+                        $uri = $script->getUri(true);
+                        $attributes = $script->getAttributes();
+
+                        echo sprintf('<ktml:script src="%s" %s />', $uri, $this->buildAttributes($attributes));
                     }
-                    $uri = $script->getUri(true);
-                    $attributes = $script->getAttributes();
-
-                    echo sprintf('<ktml:script src="%s" %s />', $uri, $this->buildAttributes($attributes));
                 }
 
-                foreach ($manager['style'] as $style) {
-                    $uri = $style->getUri(true);
-                    $attributes = $script->getAttributes();
 
-                    echo sprintf('<ktml:style src="%s" %s />', $uri, $this->buildAttributes($attributes));
+                if (isset($manager['style'])) {
+                    foreach ($manager['style'] as $style) {
+                        $uri = $style->getUri(true);
+                        $attributes = $script->getAttributes();
+
+                        echo sprintf('<ktml:style src="%s" %s />', $uri, $this->buildAttributes($attributes));
+                    }
                 }
+
             }
 
             $head = ob_get_clean();
