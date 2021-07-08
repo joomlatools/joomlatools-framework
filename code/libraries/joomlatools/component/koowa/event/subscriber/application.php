7c03f8114a7f742829ef1240b7803b705435fd67
Jebb <jebb@timble.net>
2021-07-08 18:52:43 +0800

    #523: Backport fix from 3.5.x

--- code/libraries/joomlatools/component/koowa/event/subscriber/application.php
+++ code/libraries/joomlatools/component/koowa/event/subscriber/application.php
@@ -55,18 +55,22 @@ class ComKoowaEventSubscriberApplication extends KEventSubscriberAbstract
         $request = $this->getObject('request');
 
         $app = JFactory::getApplication();
-        if ($app->isSite())
+        if ($app->isClient('site'))
         {
-            $uri     = clone JURI::getInstance();
+            try {
+                $uri    = clone JURI::getInstance();
+                $router = JFactory::getApplication()->getRouter();
+                $result = $router->parse($uri);
 
-            $router = JFactory::getApplication()->getRouter();
-            $result = $router->parse($uri);
-
-            foreach ($result as $key => $value)
-            {
-                if (!$request->query->has($key)) {
-                    $request->query->set($key, $value);
+                foreach ($result as $key => $value)
+                {
+                    if (!$request->query->has($key)) {
+                        $request->query->set($key, $value);
+                    }
                 }
+            }
+            catch (\Exception $e) {
+                if (JDEBUG) { throw $e; }
             }
         }
 
