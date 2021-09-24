b95011b68d40bea0bc97fa2a8a1d334189cca0d6
Ercan Ozkaya <ercan@timble.net>
2021-09-24 16:30:57 +0300

    #536: Ensure $_SERVER['REMOTE_ADDR'] exists before using it

--- code/libraries/joomlatools/library/dispatcher/request/abstract.php
+++ code/libraries/joomlatools/library/dispatcher/request/abstract.php
@@ -692,7 +692,7 @@ abstract class KDispatcherRequestAbstract extends KControllerRequest implements 
 
             $address   = $addresses[0];
         }
-        else $address = $_SERVER['REMOTE_ADDR'];
+        else $address = isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : null;
 
         return $address;
     }
